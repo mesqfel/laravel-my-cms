48adf8df8e2d314ae0373ddc1f8b1b3eac255cab
mesqfel <mesquita.superlogica@gmail.com>
2017-03-26 00:59:24 -0300

    Admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', function () {
+    
+    return view('admin.index');
+
+});
+
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::resource('/admin/users', 'AdminUsersController');
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+
+	<h1>Admin</h1>
+
+
+@endsection
