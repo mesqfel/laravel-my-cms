c399b597469496efd7326a536cdcfb7fdbb8530c
Felipe Mesquita <fe.mesquita88@gmail.com>
2017-05-22 19:55:15 -0300

    Updating DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(PhotosTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
     }
 }
